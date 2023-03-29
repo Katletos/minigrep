749d9875e1c24fcbc04a919e8988ffaa243f6ffd
Mikita <40485432+Katletos@users.noreply.github.com>
2023-03-29 11:51:15 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use minigrep::*;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
+    Ok(())
+}
